d8d942066a1ae9c995c8eb7c03dc6feab44b3024
Edward Whelan <vroxing@gmail.com>
2018-09-24 15:21:51 -0400

    Initial add

---
+++ functions-01.py
@@ -0,0 +1,5 @@
+def add(n, m):
+    print(n + m)
+add(5, 10)
+add(49, 21931209382)
+add(2, 2)
---
+++ turtle-01.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+def square(size):
+    forward(size)
+    right(90)
+    forward(size)
+    right(90)
+    forward(size)
+    right(90)
+    forward(size)
+    right(90)
+
+square(32)
+right(29)
+square(199)
+input()
