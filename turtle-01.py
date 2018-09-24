da16f80b19673813cc23089cd0328466ed2186a4
Edward Whelan <vroxing@gmail.com>
2018-09-24 15:43:46 -0400

    add triangle shape

--- turtle-01.py
+++ turtle-01.py
@@ -10,7 +10,19 @@ def square(size):
     forward(size)
     right(90)
 
+def triangle(size):
+    forward(size)
+    right(120)
+    forward(size)
+    right(120)
+    forward(size)
+    right(120)
+
+
 square(32)
 right(29)
 square(199)
+circle(100)
+triangle(200)
+
 input()
