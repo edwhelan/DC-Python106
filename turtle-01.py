4109a0838e39deccdfdf1eb0e4be5d48f1566256
Edward Whelan <vroxing@gmail.com>
2018-09-25 10:01:29 -0400

    finalized triforce function for date

--- turtle-01.py
+++ turtle-01.py
@@ -17,27 +17,47 @@ def triangle(size):
     left(120)
     forward(size)
     left(120)
+penup()
+goto(-280, -280)
+pendown()
+start_loc = position()          
 
 
 def triforce(size):
     pencolor('#FF940D')
+    triangle_start1 = position()
+    penup()
+    goto(triangle_start1)
+    pendown()
     triangle(size)
+  
     forward(size)
+    triangle_start2 = position()
+    penup()
+    goto(triangle_start2)
+    pendown()
     triangle(size)
     left(120)
     forward(size)
     right(120)
+    triangle_start3 = position()
+    penup()
+    goto(triangle_start3)
+    pendown()
     triangle(size)
+    triforce(size / 2)
 
 def spiral_pattern(repeat):
     for i in range(repeat):
         forward(repeat)
         left(124)
-        
-# penup()
-# goto(-280, -280)
-# pendown()
-# triforce(600)
+
+
+triforce(300)
+penup()
+goto(current_loc)
+pendown()
+triforce(150)
 # penup()
 # goto(-280, -280)
 # pendown()
